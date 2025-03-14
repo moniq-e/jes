0b3963dd2c8896764157f8bafaddd6b36e701ec7
Monique <otavioschimith124@gmail.com>
2025-03-13 21:25:42 -0300

    new ppu bitflag

--- src/main/java/com/monique/jes/ppu/ControlRegister.java
+++ src/main/java/com/monique/jes/ppu/ControlRegister.java
@@ -2,19 +2,21 @@ package com.monique.jes.ppu;
 
 import static com.monique.jes.utils.Unsign.unsignByte;
 
+import com.monique.jes.utils.bitflag.BitFlag;
+
 public class ControlRegister {
-    private short /* u8 */ bits;
+    private BitFlag bits;
 
     public ControlRegister() {
-        bits = 0;
+        bits = new BitFlag();
     }
     
     public short /* u8 */ vramAddrIncrement() {
-        return !getBitsFlag(Flag.VRAM_ADD_INCREMENT) ? (short) 1 : (short) 32;
+        return !getBitsFlag(PPUFlag.VRAM_ADD_INCREMENT) ? (short) 1 : (short) 32;
     }
 
     public void update(short /* u8 */ data) {
-        bits = unsignByte(data);
+        bits.setBits(unsignByte(data));
     }
     
     /*
@@ -35,89 +37,11 @@ public class ControlRegister {
      * +--------- Generate an NMI at the start of the
      *            vertical blanking interval (0: off; 1: on)
      */
-    public void setBitsFlag(Flag flag, boolean value) {
-        switch (flag) {
-            case GENERATE_NMI: // 1000 0000
-                if (value) {
-                    bits |= 0x80;
-                } else {
-                    bits &= 0x7F;
-                }
-                break;
-            case MASTER_SLAVE_SELECT: // 0100 0000
-                if (value) {
-                    bits |= 0x40;
-                } else {
-                    bits &= 0xBF;
-                }
-                break;
-            case SPRITE_SIZE: // 0010 0000
-                if (value) {
-                    bits |= 0x20;
-                } else {
-                    bits &= 0xDF;
-                }
-                break;
-            case BACKGROUND_PATTERN_ADDR: // 0001 0000
-                if (value) {
-                    bits |= 0x10;
-                } else {
-                    bits &= 0xEF;
-                }
-                break;
-            case SPRITE_PATTERN_ADDR: // 0000 1000
-                if (value) {
-                    bits |= 0x8;
-                } else {
-                    bits &= 0xF7;
-                }
-                break;
-            case VRAM_ADD_INCREMENT: // 0000 0100
-                if (value) {
-                    bits |= 0x4;
-                } else {
-                    bits &= 0xFB;
-                }
-                break;
-            case NAMETABLE2: // 0000 0010
-                if (value) {
-                    bits |= 0x2;
-                } else {
-                    bits &= 0xFD;
-                }
-                break;
-            case NAMETABLE1: // 0000 0001
-                if (value) {
-                    bits |= 0x1;
-                } else {
-                    bits &= 0xFE;
-                }
-                break;
-            default:
-                break;
-        }
+    public void setBitsFlag(PPUFlag flag, boolean value) {
+        bits.setBitFlag(flag, value);
     }
 
-    public boolean getBitsFlag(Flag flag) {
-        return switch (flag) {
-            case GENERATE_NMI: // 1000 0000
-                yield (bits & 0x80) != 0;
-            case MASTER_SLAVE_SELECT: // 0100 0000
-                yield (bits & 0x40) != 0;
-            case SPRITE_SIZE: // 0010 0000
-                yield (bits & 0x20) != 0;
-            case BACKGROUND_PATTERN_ADDR: // 0001 0000
-                yield (bits & 0x10) != 0;
-            case SPRITE_PATTERN_ADDR: // 0000 1000
-                yield (bits & 0x8) != 0;
-            case VRAM_ADD_INCREMENT: // 0000 0100
-                yield (bits & 0x4) != 0;
-            case NAMETABLE2: // 0000 0010
-                yield (bits & 0x2) != 0;
-            case NAMETABLE1: // 0000 0001
-                yield (bits & 0x1) != 0;
-            default:
-                yield false;
-        };
+    public boolean getBitsFlag(PPUFlag flag) {
+        return bits.getBitFlag(flag);
     }
 }
---
+++ src/main/java/com/monique/jes/ppu/PPUFlag.java
@@ -0,0 +1,26 @@
+package com.monique.jes.ppu;
+
+import com.monique.jes.utils.bitflag.Flag;
+import com.monique.jes.utils.bitflag.InnerFlag;
+
+public enum PPUFlag implements Flag {
+    GENERATE_NMI(InnerFlag.F1),
+    MASTER_SLAVE_SELECT(InnerFlag.F2),
+    SPRITE_SIZE(InnerFlag.F3),
+    BACKGROUND_PATTERN_ADDR(InnerFlag.F4),
+    SPRITE_PATTERN_ADDR(InnerFlag.F5),
+    VRAM_ADD_INCREMENT(InnerFlag.F6),
+    NAMETABLE2(InnerFlag.F7),
+    NAMETABLE1(InnerFlag.F8);
+
+    private final InnerFlag FLAG;
+
+    private PPUFlag(InnerFlag flag) {
+        FLAG = flag;
+    }
+
+    @Override
+    public InnerFlag getFlag() {
+        return FLAG;
+    }
+}
